eafc6cde94ecb16b919351fbdbf762167cd59131
Michael Wieczorkowski <mwieczorkowski@gmail.com>
2021-12-08 13:51:42 -0500

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "../components/header/header";
+import HeaderNoSidebar from "../components/header/headerNoSidebar";
+import Footer from "../components/footer/footer";
+import '../scss/style.scss';
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className="main-wrapper">
+        <Header />
+        <HeaderNoSidebar />
+        <div className="home-content-wrapper">
+            <main>{children}</main>
+            <Footer />
+        </div>
+        
+    </div>
+  )
+}
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
+
---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Banner from "../components/homedefault/banner";
+import About from "../components/homedefault/about";
+import Service from "../components/homedefault/service";
+import Project from "../components/homedefault/project";
+import Testimonial from "../components/homedefault/testimonial";
+import Timeline from "../components/homedefault/timeline";
+import BlogPost from "../components/blogPost";
+import Contact from "../elements/contact/contact";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Michael Wieczorkowski" />
+    <Banner />
+    <About />
+    <Service />
+    <div id="portfolio">
+        <Project />
+        <Testimonial />
+        <Timeline />
+    </div>
+    <BlogPost />
+    <Contact />
+  </Layout>
+
+)
+export default IndexPage;
