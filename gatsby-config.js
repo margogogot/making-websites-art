9fb6b10432ba0e0c91bbc18d5269a3d656c60603
Michael Wieczorkowski <mwieczorkowski@gmail.com>
2021-12-13 08:55:17 -0500

    update gtag params

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,6 @@ module.exports = {
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },
@@ -86,7 +85,7 @@ module.exports = {
           // Puts tracking script in the head instead of the body
           head: true,
           // Setting this parameter is also optional
-          respectDNT: true,
+          respectDNT: false,
           // Avoids sending pageview hits from custom paths
           exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
