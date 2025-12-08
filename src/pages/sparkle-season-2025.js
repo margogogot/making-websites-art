49f028c99dc2f73824399db6c4443203dc12ea48
Margot Wieczorkowski <mwieczorkowski@gmail.com>
2025-12-08 18:28:31 -0500

    update sparkle season message

---
+++ src/pages/sparkle-season-2025.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Sparkle Season 2025" />
+    <section>
+        <p>You're too early! Do you shake your presents before Christmas, too?</p>
+        <img src="./images/data.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
+    </section>
+  </Layout>
+
+)
+export default IndexPage;
