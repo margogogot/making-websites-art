a4ed43c1d2cddbbb00881426e120a69d54d36a01
Margot Wieczorkowski <mwieczorkowski@gmail.com>
2025-12-08 18:36:04 -0500

    update sparkle page

--- src/pages/sparkle-season-2025.js
+++ src/pages/sparkle-season-2025.js
@@ -4,13 +4,13 @@ import SEO from "../components/seo";
 
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Sparkle Season 2025" />
-    <section>
-        <p>You're too early! Do you shake your presents before Christmas, too?</p>
-        <img src="./images/data.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
-    </section>
-  </Layout>
+    <Layout>
+        <SEO title="Sparkle Season 2025" />
+        <section style={{ minHeight: '100dvh', display: "flex", flexDirection: "column", gap: "2rem" }}>
+            <p>You're too early! Do you shake your presents before Christmas, too?</p>
+            <img src="./img/shaking-present.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
+        </section>
+    </Layout>
 
 )
 export default IndexPage;
