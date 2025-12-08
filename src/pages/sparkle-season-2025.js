2713f2561a615c0d74b619ab3da20c138ef21013
Margot Wieczorkowski <mwieczorkowski@gmail.com>
2025-12-08 18:40:56 -0500

    Update sparkle-season-2025.js

--- src/pages/sparkle-season-2025.js
+++ src/pages/sparkle-season-2025.js
@@ -7,8 +7,11 @@ const IndexPage = () => (
     <Layout>
         <SEO title="Sparkle Season 2025" />
         <section style={{ minHeight: '100dvh', display: "flex", flexDirection: "column", gap: "2rem" }}>
-            <p>You're too early! Do you shake your presents before Christmas, too?</p>
-            <img src="./img/shaking-present.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
+            <div style={{ maxWidth: "600px" }}>
+                <p>You're too early! Do you shake your presents before Christmas, too?</p>
+                <img src="/img/shaking-present.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
+            </div>
+
         </section>
     </Layout>
 
