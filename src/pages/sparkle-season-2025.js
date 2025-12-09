0276534c2b65f1129d86093032d81a4f48fe6a8e
Margot Wieczorkowski <mwieczorkowski@gmail.com>
2025-12-09 09:25:28 -0500

    Update sparkle-season-2025.js

--- src/pages/sparkle-season-2025.js
+++ src/pages/sparkle-season-2025.js
@@ -18,21 +18,44 @@ const getCountDownDate = () => {
     return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds until Christmas 2025.`
 }
 
+const getDaysTilChristmas = () => {
+    var now = new Date().getTime();
+
+    // Find the distance between now and Christmastime
+    var distance = Christmas - now;
+    return Math.floor(distance / (1000 * 60 * 60 * 24));
+
+}
+
 const IndexPage = () => {
     const [countDownMesssage, setCountDownMessage] = useState(getCountDownDate());
+    const [daysTilChristmas, setDaysTilChristmas] = useState()
     setInterval(() => {
         setCountDownMessage(getCountDownDate());
+        setDaysTilChristmas(getDaysTilChristmas());
     }, 1000)
     return (<Layout>
         <SEO title="Sparkle Season 2025" />
-        <section style={{ minHeight: '100dvh', display: "flex", flexDirection: "column", gap: "2rem", justifyContent: "center", alignItems: "center" }}>
-            <div style={{ maxWidth: "600px" }}>
-                <p style={{ fontWeight: "bold" }}>You're too early! Do you shake your presents before Christmas, too?</p>
-                <figure>
-                    <img src="/img/shaking-present.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
-                    <caption style={{width: "100%"}}>{countDownMesssage}</caption>
-                </figure>
-            </div>
+        <section style={{ minHeight: '100dvh', display: "flex", flexDirection: "column", gap: "2rem", padding: "2rem", justifyContent: "center", alignItems: "center" }}>
+            {daysTilChristmas > 2 &&
+                <div style={{ maxWidth: "600px" }}>
+                    <p style={{ fontWeight: "bold" }}>You're too early! Do you shake your presents before Christmas, too?</p>
+                    <figure>
+                        <img src="/img/shaking-present.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
+                        <caption style={{ width: "100%" }}>{countDownMesssage}</caption>
+                    </figure>
+                </div>
+            }
+            {daysTilChristmas <= 2 &&
+                <div style={{ maxWidth: "600px" }}>
+                    {daysTilChristmas > 0 &&
+                        <p style={{ fontWeight: "bold" }}>Okay, you can have an early treat.</p>
+                    }
+                    {daysTilChristmas < 1 &&
+                        <p style={{ fontWeight: "bold" }}>It's Christmas Day!</p>
+                    }
+                </div>
+            }
 
         </section>
     </Layout>)
