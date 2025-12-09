7522c8a2aa5b59bc72c068d74d09e04ca98e6e3d
Margot Wieczorkowski <mwieczorkowski@gmail.com>
2025-12-08 19:02:09 -0500

    Update sparkle-season-2025.js

--- src/pages/sparkle-season-2025.js
+++ src/pages/sparkle-season-2025.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+const Christmas = new Date("Dec 25, 2025 00:00:00").getTime();
+const getCountDownDate = () => {
+    var now = new Date().getTime();
 
-const IndexPage = () => (
-    <Layout>
+    // Find the distance between now and Christmastime
+    var distance = Christmas - now;
+
+    // Get days, hours, minutes, seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} until Christmas 2025.`
+}
+
+const IndexPage = () => {
+    const [countDownMesssage, setCountDownMessage] = useState(getCountDownDate());
+    setInterval(() => {
+        setCountDownMessage(getCountDownDate());
+    }, 1000)
+    return (<Layout>
         <SEO title="Sparkle Season 2025" />
         <section style={{ minHeight: '100dvh', display: "flex", flexDirection: "column", gap: "2rem", justifyContent: "center", alignItems: "center" }}>
             <div style={{ maxWidth: "600px" }}>
-                <p style={{fontWeight: "bold"}}>You're too early! Do you shake your presents before Christmas, too?</p>
+                <p style={{ fontWeight: "bold" }}>You're too early! Do you shake your presents before Christmas, too?</p>
                 <img src="/img/shaking-present.jpg" alt="picture of a nerd shaking a Christmas gift. That is you."></img>
+                <p>{countDownMesssage}</p>
             </div>
 
         </section>
-    </Layout>
+    </Layout>)
+}
+    
 
 )
 export default IndexPage;
